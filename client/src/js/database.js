b3e9cf07f43ba0410746334f01ac840e64ca2980
Daniel Lawrence <dan.lawrence0810@gmail.com>
2022-04-21 20:43:08 +0930

    modules installed, idb created

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,25 +1,28 @@
 // TODO: Install the following package:
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 // TODO: Complete the initDb() function below:
 const initdb = async () => {
+  openDB("contacts", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("contacts")) {
+        console.log("contacts database already exits");
+        return;
+      }
 
+      db.createObjectStore("contacts", { keyPath: "id", autoIncrement: true });
+      console.log("contacts database created");
+    },
+  });
 };
-
 
 // TODO: Complete the postDb() function below:
-export const postDb = async (name, home, cell, email)  => {
- 
-};
+export const postDb = async (name, home, cell, email) => {};
 
 // TODO: Complete the getDb() function below:
-export const getDb = async () => {
-  
-};
+export const getDb = async () => {};
 
 // TODO: Complete the deleteDb() function below:
-export const deleteDb = async (id) => {
-  
-};
+export const deleteDb = async (id) => {};
 
 initdb();
